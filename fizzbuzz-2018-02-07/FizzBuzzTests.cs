214098b117117cf3aab8363e554332296e5f4a83
Jim Speaker <jim@IONTECH.org>
2018-02-07 20:49:32 -0800

    done kata

---
+++ fizzbuzz-2018-02-07/FizzBuzzTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace fizzbuzz_2018_02_07
+{
+  [TestFixture]
+  public class FizzBuzzTests
+  {
+    [Test, Category("Unit")]
+    public void GivenTwo_WhenAskingToString_ThenItShouldReturnTwo()
+    {
+      // arrange
+      IInteger integer = new Integer(2);
+
+      // act
+      string two = integer.ToString();
+
+      // assert
+      two.Should().Be("2");
+    }
+
+    [Test, Category("Unit")]
+    public void GivenSix_WhenAskingToString_ThenItShouldReturnFizz()
+    {
+      // arrange
+      IInteger integer = new Integer(6);
+
+      // act
+      string fizz = integer.ToString();
+
+      // assert
+      fizz.Should().Be("fizz");
+    }
+ 
+    [Test, Category("Unit")]
+    public void GivenTen_WhenAskingToString_ThenItShouldReturnBuzz()
+    {
+      // arrange
+      IInteger integer = new Integer(10);
+
+      // act
+      string buzz = integer.ToString();
+
+      // assert
+      buzz.Should().Be("buzz");
+    }
+ 
+    [Test, Category("Unit")]
+    public void GivenThirty_WhenAskingToString_ThenItShouldReturnFizzBuzz()
+    {
+      // arrange
+      IInteger integer = new Integer(30);
+
+      // act
+      string fizzbuzz = integer.ToString();
+
+      // assert
+      fizzbuzz.Should().Be("fizzbuzz");
+    }
+  }
+
+  public interface IInteger
+  {
+    string ToString();
+  }
+
+  public class Integer : IInteger
+  {
+    private readonly int _value;
+
+    public Integer(int value)
+    {
+      _value = value;
+    }
+
+    public override string ToString()
+    {
+      const string fizz = "fizz";
+      const string buzz = "buzz";
+
+      if (_value.DivisibleBy(15)) return $"{fizz}{buzz}";
+      if (_value.DivisibleBy(5)) return buzz;
+      if (_value.DivisibleBy(3)) return fizz;
+      return _value.ToString();
+    }
+  }
+
+  public static class IntExtensions
+  {
+    public static bool DivisibleBy(this int dividend, int divisor)
+    {
+      return dividend % divisor == 0;
+    }
+  }
+}
---
+++ fizzbuzz-2018-02-07/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("fizzbuzz_2018_02_07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fizzbuzz_2018_02_07")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46ba9275-983e-4310-b2f7-755878f44b39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
